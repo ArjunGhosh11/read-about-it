30e10bc1a5563a235207b8c621cf25cab39d98ef
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-04 17:35:56 +0600

    Components added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import NotFound from './components/NotFound/NotFound';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/home' element={<Home></Home>}></Route>
           <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+          <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
 
         </Routes>
       </header>
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Dashboard.css'
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>This the dashboard</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
